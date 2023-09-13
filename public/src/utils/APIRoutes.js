6179aabb57f3fe53a3168e5b82c18a59367016a0
Yash Singh <108719370+YashDxr@users.noreply.github.com>
2023-09-13 08:52:53 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+//export const host = "http://localhost:5000";
+export const host = "https://chatappbackend-qg1f.onrender.com:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
